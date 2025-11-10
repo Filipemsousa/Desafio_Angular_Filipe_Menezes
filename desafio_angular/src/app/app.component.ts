5ca23e97490fc26eb12c6f64b306ff0058fb079c
Filipemsousa <filipmenzessousa@gmail.com>
2025-11-10 11:40:27 -0300

    paginas no angular

--- desafio_angular/src/app/app.component.ts
+++ desafio_angular/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { IndexComponent } from './componentes/index/index.component';
 import { HomeComponent } from "./componentes/home/home.component";
+import { DashboardComponent } from './componentes/dashboard/dashboard.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, IndexComponent, HomeComponent],
+  imports: [RouterOutlet, IndexComponent, HomeComponent,DashboardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- desafio_angular/src/app/app.routes.ts
+++ desafio_angular/src/app/app.routes.ts
@@ -1,10 +1,13 @@
 import { Routes } from '@angular/router';
 import { IndexComponent } from './componentes/index/index.component';
 import { HomeComponent } from './componentes/home/home.component';
+import { DashboardComponent } from './componentes/dashboard/dashboard.component';
 
 export const routes: Routes = [
     {path:"index",component:IndexComponent},
     {path:"home",component:HomeComponent},
+    {path:"dashboard",component:DashboardComponent},
     {path:"",redirectTo:"home",pathMatch:"full"},
-    {path:"",redirectTo:"index",pathMatch:"full"}
+    {path:"",redirectTo:"index",pathMatch:"full"},
+    {path:"",redirectTo:"dashboard",pathMatch:"full"}
 ];
